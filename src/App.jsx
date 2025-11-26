f851d618c973c92b6ac0ad8000f0e36c47182787
Cody Keith <codykeith9@gmail.com>
2025-11-26 10:03:02 -0500

    casing issue for render

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Layout from "./components/layout.jsx";
+import Layout from "./components/Layout.jsx";
 import Hero from "./components/Hero.jsx";
 import LocalHostingStrip from "./components/LocalHostingStrip.jsx";
 import ServicesSection from "./components/ServicesSection.jsx";
